a62121cdbb939994347238ec5f8f1be8f018732b
Glen Yu <glen.yu@gmail.com>
2025-06-23 10:11:35 -0400

    updates
    Signed-off-by: Glen Yu <glen.yu@gmail.com>

--- server.py
+++ server.py
@@ -1,29 +1,104 @@
+"""This module defines a FastMCP server for Google Maps Platform operations."""
 import os
-import re 
+import re
 import json
+import asyncio
 import googlemaps
-
+from fastmcp import FastMCP
 from typing import Optional, Dict, Any
 
 
-#-------------
+#---------------
 # settings
-#-------------
+#---------------
+host=os.environ.get("FASTMCP_HOST", "0.0.0.0")
+port=os.environ.get("FASTMCP_PORT", 8080)
+transport=os.environ.get("FASTMCP_TRANSPORT", "stdio")  # stdio, streamable-http, sse
+
 google_maps_api_key=os.getenv("GOOGLE_MAPS_API_KEY")
 gmaps = googlemaps.Client(key=google_maps_api_key)
+
+
+#-------------
+# helpers
+#-------------
+allowed_modes = ["driving", "walking", "bicycling", "transit"]
+def assert_mode(mode: str):
+    assert mode in allowed_modes, f"ERROR: '{mode}' is not one of the allowed modes: {allowed_modes}"
+
+
 allowed_input_types = ["textquery", "phonenumber"]
-
-
-#-------------
-# helper
-#-------------
 def assert_input_type(input_type: str):
     assert input_type in allowed_input_types, f"ERROR: '{input_type}' is not one of the allowed inpute types: {allowed_input_types}"
 
 
-#-------------
-# mcp
-#-------------
+def strip_html_regex(html_text: str) -> str:
+    """Strips HTML tags and reduce multiple blank spaces to one"""
+    clean = re.compile('<.*?>')
+    temp = re.sub(clean, ' ', html_text)
+    final = re.sub(r' {2,}', ' ', temp)
+    return final
+
+
+#------------------
+# initialize fastmcp
+#------------------
+# https://gofastmcp.com/servers/fastmcp#server-configuration
+# Initialize FastMCP server
+mcp = FastMCP(
+    name="FastMCP Google Maps Platform Server",
+    dependencies=["googlemaps==4.10.0", "asyncio==3.4.3"],
+    on_duplicate_tools="error",
+)
+
+
+#-------------------
+# direction tools
+#-------------------
+@mcp.tool()
+def get_directions(origin: str, destination: str, mode: str="driving") -> Optional[Dict[str, Any]]:
+    """Gives step-by-step instructions to get from origin to destination uisng a particular mode of transport
+    Args:
+        origin (str): originating address
+        destination (str): destination address
+        mode (str, optional): mode of transportation. Valid options are: driving, walking, bicycling, transit
+
+    Returns:
+        dictionary (JSON) of steps along with total distance and total duration
+    """
+    try:
+        assert_mode(mode)
+    except AssertionError as e:
+        print(e)
+
+    results = gmaps.directions(origin, destination, mode=mode)
+
+    if results:
+        route = results[0]
+
+        output = {
+            "total_distance": route['legs'][0]['distance']['text'],
+            "total_duration": route['legs'][0]['duration']['text'],
+            "steps": []
+        }
+
+        for leg in route['legs']:
+            for step in leg['steps']:
+                output["steps"].append({
+                    "instruction": strip_html_regex(step['html_instructions']),
+                    "distance": step['distance']['text'],
+                    "duration": step['duration']['text']
+                })
+        #return json.dumps(output, indent=4)
+        return json.dumps(output, separators=(',', ':'))
+    else:
+        return "No directions found for the specified locations."
+
+
+#-------------------
+# places tools
+#-------------------
+@mcp.tool()
 def find_place(input: str, input_type: str="textquery", fields: list=["place_id", "formatted_address", "name", "geometry", "types", "rating"]) -> Optional[Dict[str, Any]]:
     """Find/query a place with the provided name
     Args:
@@ -64,6 +139,7 @@ def find_place(input: str, input_type: str="textquery", fields: list=["place_id"
         return json.dumps(output, separators=(',', ':'))
 
 
+@mcp.tool()
 def place_nearby(location: dict, radius: int, place_type: str) -> Optional[Dict[str, Any]]:
     """Find types of places within a radius of a location (latitude, longitude)
     Args:
@@ -93,6 +169,7 @@ def place_nearby(location: dict, radius: int, place_type: str) -> Optional[Dict[
         return "Nothing nearby that matches the search criteria was found."
 
 
+@mcp.tool()
 def place_details(place_id: str, fields: list=["name", "formatted_address", "formatted_phone_number", "geometry", "website", "types", "rating", "user_ratings_total"]) -> Optional[Dict[str, Any]]:
     """
     Args:
@@ -130,18 +207,16 @@ def place_details(place_id: str, fields: list=["name", "formatted_address", "for
         return "No such place found."
 
 
-#print(find_place("Engineering 7, Waterloo"))
-print("FIND PLACE")
-print(find_place("+14162928742", input_type="phonenumber"))
-print(find_place("Googleplex"))
-print(find_place("Coffee shop")) # if you don't specify, it will use your current location
-print(find_place("My current location"))
-#print(find_place("Alton Chinese Restaurant, Scarborough"))
-#print(find_place("Tim Horton's near 30 Muirlands Drive"))
-#print(find_place("Random temple, Wakanda"))
-print("PLACE DETAILS")
-print(place_details("ChIJr6vaxLHW1IkRgrdDqsWyvN4"))
-
-print("PLACES NEARBY")
-#print(place_nearby({"lat": 43.825212,"lng":-79.27592589999999}, "2000", "italian restaurant"))
-print(place_nearby({"lat":43.4730414,"lng":-80.53952389999999}, "1000", "hotel"))
+#----------------
+# main
+#----------------
+# https://gofastmcp.com/deployment/running-server
+if __name__ == "__main__":
+    try:
+        asyncio.run(mcp.run(transport=transport, host=host, port=port))
+    except KeyboardInterrupt:
+        print("> FastMCP Google Maps Server stopped by user.")
+    except Exception as e:
+        print(f"> FastMCP Google Maps Server encountered error: {e}")
+    finally:
+        print("> FastMCP Google Maps Server exiting.")
